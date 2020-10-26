17596394ffc1e3a06ddb3a10a96f9e9f94fa1c62
SongMinSu <songminsu2363@gmail.com>
2020-10-26 21:56:52 +0900

    10/26일 백준 알고리즘

---
+++ venv/Num1000.py
@@ -0,0 +1,9 @@
+#Num1000.py
+'''
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+첫째 줄에 A+B를 출력한다.
+'''
+a,b = map(int,input().split())
+if (a>0 and a<10) and (b>0 and b<10):
+    print(a+b)

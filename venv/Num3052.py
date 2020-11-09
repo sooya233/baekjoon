6d3b5a54dea9a6ec2f012e3cc53846cda1b52164
SongMinSu <songminsu2363@gmail.com>
2020-11-09 22:44:51 +0900

    2020/11/9 파이썬공부

---
+++ venv/Num3052.py
@@ -0,0 +1,30 @@
+#Num3052.py
+
+'''
+두 자연수 A와 B가 있을 때, A%B는 A를 B로 나눈 나머지 이다. 예를 들어, 7, 14, 27, 38을 3으로 나눈 나머지는 1, 2, 0, 2이다.
+수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한다. 그 다음 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
+
+첫째 줄부터 열번째 줄 까지 숫자가 한 줄에 하나씩 주어진다. 이 숫자는 1,000보다 작거나 같고, 음이 아닌 정수이다.
+
+첫째 줄에, 42로 나누었을 때, 서로 다른 나머지가 몇 개 있는지 출력한다.
+'''
+remainder = []
+count = [0 for i in range(42)]
+remainder_count = 0
+
+
+for i in range(10):
+    a = int(input())
+    if(a>=0 and a<=1000):
+        remainder.append(a%42)
+
+#print(remainder)
+
+for j in remainder:
+    count[j] += 1
+
+for k in count:
+    if(k!=0):
+        remainder_count += 1
+
+print(remainder_count)

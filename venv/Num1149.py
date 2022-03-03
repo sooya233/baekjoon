dba59646da0f87362799174d604edcc3327f3ff2
SongMinSu <sooya233@naver.com>
2022-03-03 15:52:31 +0900

    2020/03/03 problem

---
+++ venv/Num1149.py
@@ -0,0 +1,15 @@
+# Num1149.py
+# https://www.acmicpc.net/problem/1149
+
+n = int(input())
+dp = []
+
+for i in range(n):
+    dp.append(list(map(int, input().split())))
+
+for i in range(1, len(dp)):
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + dp[i][0]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + dp[i][1]
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + dp[i][2]
+
+print(min(dp[n-1][0], dp[n-1][1], dp[n-1][2]))
